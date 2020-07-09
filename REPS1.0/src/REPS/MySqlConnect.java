6745e023947198b0fc3f8687f740d36354ab509e
Yashika Varshney <36771482+yyyashikaaa@users.noreply.github.com>
2020-07-09 16:29:29 +0530

    Add files via upload

---
+++ REPS1.0/src/REPS/MySqlConnect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package REPS;
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author lapi
+ */
+public class MySqlConnect {
+    Connection conn=null;
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/admin?zeroDateTimeBehavior=convertToNull","root","");
+           // JOptionPane.showMessageDialog(null,"Connected to database");
+            return conn;
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
